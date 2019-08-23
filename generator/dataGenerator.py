b9d97eb8b01681ea28c4e69a691fc11f0e368b68
H3adlock <sourav.ghosh700@hotmail.com>
2019-08-23 16:48:01 +0530

    0003: datageneratore working

--- generator/dataGenerator.py
+++ generator/dataGenerator.py
@@ -1,7 +1,9 @@
 # xlsxFileName = input('Enter the xlsx file name: ')
 # payloadFile = input('Enter the payload file name: ')
+import random
 xlsxFileName = "test_data.xlsx"
 payloadFile = "eval_payload.txt"
+propertyFile = "properties.xlsx"
 payload_list = list()
 
 if __name__ == '__main__':
@@ -10,24 +12,38 @@ if __name__ == '__main__':
         from os import path
         sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
         from xlsxImport.pandasImport import Pandasimport
+        from xlsxImport.pandasexport import Pandasexport
         from fileUtilities.fileUtility import fileToList
         try:
-            userData = Pandasimport.retrieve_xlsx(xlsxFileName)
+            testData = Pandasimport.retrieve_xlsx(xlsxFileName)
+            propertyData = Pandasimport.retrieve_xlsx(propertyFile)
             payload_list = fileToList.readFileToList(payloadFile)
         except:
-            print('Check if {} & {} files exist in dataFolder..'.format(
-                xlsxFileName, payloadFile))
+            print('Check if {} ,{} & {} files exist in dataFolder..'.format(
+                xlsxFileName, propertyData, payloadFile))
 else:
     from ..xlsxImport.pandasImport import Pandasimport
     from ..fileUtilities.fileUtility import fileToList
     try:
-        userData = Pandasimport.retrieve_xlsx(xlsxFileName)
+        testData = Pandasimport.retrieve_xlsx(xlsxFileName)
+        propertyData = Pandasimport.retrieve_xlsx(propertyFile)
         payload_list = fileToList.readFileToList(payloadFile)
     except:
-        print('Check if {} & {} files exist in dataFolder..'.format(
-            xlsxFileName, payloadFile))
+        print('Check if {}, {} & {} files exist in dataFolder..'.format(
+            xlsxFileName, propertyData, payloadFile))
 
-print(userData['userName'])
-userData
-for entry in payload_list:
-    print(entry)
+# print(testData['userName'])
+# for i in range(len(payload_list)):
+#     print(random.choice(payload_list))
+print(propertyData)
+
+for ind in propertyData.index:
+    if propertyData['header'][ind] in testData.columns:
+        if propertyData['action'][ind] == 'append':
+            testData[propertyData['header'][ind]] = testData[propertyData['header'][ind]].apply(
+                lambda x: str(x) + random.choice(payload_list))
+        elif propertyData['action'][ind] == 'replace':
+            testData[propertyData['header'][ind]] = testData[propertyData['header'][ind]].apply(
+                lambda x: random.choice(payload_list))
+
+print(testData)
---
+++ xlsxImport/pandasexport.py
@@ -0,0 +1,19 @@
+import os
+try:
+    import pandas as pd
+    import xlrd
+except:
+    os.system('pip install pandas')
+    os.system('pip install xlrd')
+    import pandas as pd
+    import xlrd
+
+
+class Pandasexport:
+    def retrieve_xlsx(xlsxDataFrame):
+        current_file = os.path.abspath(os.path.dirname(__file__))
+        excelPath = os.path.join(current_file, '../dataFolder/', xlsxFileName)
+        user_xlsx = pd.ExcelFile(excelPath)
+        user_data = pd.read_excel(
+            user_xlsx, 'Sheet1', index_col=None, na_values=['NA'])
+        return user_data
