de1562f2e7ebfb58d2cb96ac9e5e6e5fbbab7f8f
H3adlock <sourav.ghosh700@hotmail.com>
2019-08-23 10:41:25 +0530

    0002: main module change for package import

--- generator/dataGenerator.py
+++ generator/dataGenerator.py
@@ -2,9 +2,9 @@
 # payloadFile = input('Enter the payload file name: ')
 xlsxFileName = "test_data.xlsx"
 payloadFile = "eval_payload.txt"
+payload_list = list()
 
 if __name__ == '__main__':
-    payload_list = list()
     if __package__ is None:
         import sys
         from os import path
@@ -17,17 +17,17 @@ if __name__ == '__main__':
         except:
             print('Check if {} & {} files exist in dataFolder..'.format(
                 xlsxFileName, payloadFile))
-    else:
-        from ..xlsxImport.pandasImport import Pandasimport
-        from ..fileUtilities.fileUtility import fileToList
-        try:
-            userData = Pandasimport.retrieve_xlsx(xlsxFileName)
-            payload_list = fileToList.readFileToList(payloadFile)
-        except:
-            print('Check if {} & {} files exist in dataFolder..'.format(
-                xlsxFileName, payloadFile))
+else:
+    from ..xlsxImport.pandasImport import Pandasimport
+    from ..fileUtilities.fileUtility import fileToList
+    try:
+        userData = Pandasimport.retrieve_xlsx(xlsxFileName)
+        payload_list = fileToList.readFileToList(payloadFile)
+    except:
+        print('Check if {} & {} files exist in dataFolder..'.format(
+            xlsxFileName, payloadFile))
 
-    print(userData['userName'])
-    userData
-    for entry in payload_list:
-        print(entry)
+print(userData['userName'])
+userData
+for entry in payload_list:
+    print(entry)
